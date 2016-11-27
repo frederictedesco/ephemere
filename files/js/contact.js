0330a617c3662b8951d6c0c3edf3e3f2144d54dc
Frederic Tedesco <frederic@dcode.eu>
2016-11-27 21:20:06 +0100

    add contact form that uses RobinHook framework

--- files/js/contact.js
+++ files/js/contact.js
@@ -1,22 +1,9 @@
-var baseUrl = "http://www.galerie-ephemere-liege.be/es/ephemere/contact"
-
-function statusDcode(){
-    baseUrl.get(function (arg) {
-         console.log(arg);
-        }
-    );
-};
-
-function status(e){alert(e)};
-
 function postContact(){
 	var name = $("#name").val();
 	var email = $("#email").val();
 	var phone = $("#phone").val();
 	var message = $("#message").val();
-	baseUrl.post(
-		function(e){},
-		function(e){},
-		{"name":name,"email":email,"phone":phone,"message":message},
-		1000);
+	ROBINHOOK.push({'event': 'TrackEvent','site':'ephemere','name':name,'email':email,'phone':phone,'message':message, 'eventCategory' :  'Social',  'eventAction' :  'post_contact_form'});
+    $(".success-message").show();
+    $(".send-btn").hide();
 };
